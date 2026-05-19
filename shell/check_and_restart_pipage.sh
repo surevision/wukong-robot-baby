c26aa57661761d31c2614be50010f93bd0aae3ce
surevision <surevision@163.com>
2026-05-19 12:56:37 +0800

    新环境

--- shell/check_and_restart_pipage.sh
+++ shell/check_and_restart_pipage.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-cd /home/admin
+cd /home/pi
 
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init - bash)"
-eval "$(pyenv virtualenv-init -)"
+#export PYENV_ROOT="$HOME/.pyenv"
+#[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+#eval "$(pyenv init - bash)"
+#eval "$(pyenv virtualenv-init -)"
 
 export PYTHON_BUILD_MIRROR_URL="https://registry.npmmirror.com/-/binary/python"
 export PYTHON_BUILD_MIRROR_URL_SKIP_CHECKSUM=1
 
-# cd /home/admin/shell
+# cd /home/pi/shell
 # bash check_and_restart_wukong.sh
 
 ProcNumber=$(ps -ef | grep -w page_server | wc -l)
 if [ ${ProcNumber} -le 1 ]; then
 echo "no page_server process, try start"
-cd /home/admin/pi-page
-nohup python page_server.py &
+cd /home/pi/pi-page
+nohup /usr/bin/python page_server.py &
 else
-echo "tailscale already online"
+echo "page_server already online"
 fi
 sleep 1s
 
--- shell/check_and_restart_wukong.sh
+++ shell/check_and_restart_wukong.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd /home/admin
+cd /home/pi
 
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init - bash)"
-eval "$(pyenv virtualenv-init -)"
+#export PYENV_ROOT="$HOME/.pyenv"
+#[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+#eval "$(pyenv init - bash)"
+#eval "$(pyenv virtualenv-init -)"
 
 export PYTHON_BUILD_MIRROR_URL="https://registry.npmmirror.com/-/binary/python"
 export PYTHON_BUILD_MIRROR_URL_SKIP_CHECKSUM=1
@@ -14,9 +14,9 @@ ProcNumber=`ps -ef | grep -w wukong.py | grep -v grep | wc -l`
 echo ${ProcNumber}
 if [ ${ProcNumber} -le 1 ]; then
 echo "no wukong process, try start"
-cd /home/admin
-cd /home/admin/wukong-robot
-nohup python wukong.py &
+cd /home/pi
+cd /home/pi/wukong-robot
+nohup /usr/bin/python wukong.py &
 echo "wukong start called"
 else
 echo "wukong already online"
